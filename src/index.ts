2551e5ddfe9391b48f6951e04525cb94489ace97
nitrocode <alexidowu25@gmail.com>
2020-08-13 21:44:23 +0100

    refactor(github): workflow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export const sum = (a: number, b: number): number => a + b
 export const sub = (a: number, b: number): number => a - b
-export const div = (a: number, b: number): number => a / b
+export const div = (a: number, c: number): number => a / c
 export const mult = (a: number, b: number): number => a * b
