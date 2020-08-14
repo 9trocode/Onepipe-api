5d7de7c389dd862024da7c1a4bb2c53953f30a6b
nitrocode <alexidowu25@gmail.com>
2020-08-14 11:26:54 +0100

    bug(module): fixed module declaration

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { expect } from 'chai'
-import OnePipeSdk from '../src'
+import OnePipe from '../src'
 
 describe('calc tests', () => {
 	describe('sum()', () => {
 		context('when it receives "a" = 2 and "b" = 1', () => {
 			it('should return 3', () => {
-				const response = OnePipeSdk('677', '7837438')
+				const response = OnePipe('677', 'gghh')
 				const data = response.v2.AccountServices.OpenAccount({
 					transaction: {},
 					// eslint-disable-next-line @typescript-eslint/camelcase
